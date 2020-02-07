13b87b8b8ecb80249030976a6ab13f1d6a0ad213
huancnttuet <huancnttuet@gmail.com>
2020-02-07 15:44:15 +0700

    update

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345678@localhost:5432/cars_api"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345678@localhost:5432/cars_api"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ehcvizsztwxxfy:84d3521c4223cff25a51567d24651252e2cd98d70abaec4f8a4f6fa3d438abcb@ec2-184-72-236-57.compute-1.amazonaws.com:5432/d99nfdrao8b0ah"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
