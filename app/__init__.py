35fd92ee1527b954f5dd1c6a8b0237fcb6816fea
huancnttuet <huancnttuet@gmail.com>
2020-04-18 08:55:09 +0700

    update db

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     # ] = "postgresql://postgres:12345678@localhost:5432/uet_api"
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = "postgres://ehcvizsztwxxfy:84d3521c4223cff25a51567d24651252e2cd98d70abaec4f8a4f6fa3d438abcb@ec2-184-72-236-57.compute-1.amazonaws.com:5432/d99nfdrao8b0ah"
+    ] = "postgres://lxzerkyuvpmqcq:01cfd513d5a5ae92bc3ffd6fd07f34e7ce6c145d805911dc1f51cd2335e0baf2@ec2-3-231-46-238.compute-1.amazonaws.com:5432/d3164254kl09mu"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     models.init_app(app)
