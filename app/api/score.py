727c677111468e7ac7969f34d5549dbcc551a8fb
huancnttuet <huancnttuet@gmail.com>
2020-06-10 23:37:44 +0700

    @@

--- app/api/score.py
+++ app/api/score.py
@@ -81,8 +81,8 @@ class SearchSimple(Resource):
         term = args.term
         type_education = args.type_education
         results = uet_api.search(text, term, type_education)
-        results[1] = [[results[1]]]
-        return results
+
+        return {"term_name": results[1], "subject_list": results[0]}
 
 
 @score_ns.route("/quickSearch")
