36d59de0b434055fa5951a38b0e577833a33d04f
huancnttuet <huancnttuet@gmail.com>
2020-06-13 22:27:28 +0700

    ^^

--- app/api/exam_time.py
+++ app/api/exam_time.py
@@ -16,4 +16,21 @@ class ExamTimeSimple(Resource):
         args = requests.exam_time_list_params.parse_args()
         mssv = args.mssv
         results = uet_qldt_api.get_exam_time(mssv)
-        return results
+        re = []
+        for r in results:
+            re.append({
+                'id': r[0],
+                'student_code': r[1],
+                'student_name': r[2],
+                'class_name': r[3],
+                'sbd': r[4],
+                'course_code': r[5],
+                'course_name': r[6],
+                'day': r[7],
+                'time': r[8],
+                'exam_case': r[9],
+                'room': r[10],
+                'place': r[11],
+                'note': r[12]}
+            )
+        return re
